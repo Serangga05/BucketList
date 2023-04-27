47e938ef9d9de3e973e05f152b2819f725aa3be7
unknown <serangrangga@gmail.com>
2023-04-27 10:02:14 +0700

    First Commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
+from datetime import datetime
 
 import os
 from os.path import join, dirname
@@ -18,40 +19,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
-    count = db.bucket.count_documents({})
-    num = count + 1
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({},{'_id':False}))
+    return jsonify({'articles': articles})
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+    
+    today = datetime.now()
+    mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
+    tanggal = today.strftime('%Y-%m-%d')
+
+    file = request.files['file_give']
+    extension = file.filename.split('.')[-1]
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}'
+    profile.save(profilename)
+
+
     doc = {
-        'num' : num,
-        'bucket' : bucket_receive,
-        'done' : 0
+        'file' : filename,
+        'profile' : profilename,
+        'title' : title_receive,
+        'content' : content_receive,
+        'tanggal' : tanggal
     }
-    db.bucket.insert_one(doc)
-    return jsonify({'msg': 'Data saved!'})
-
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one(
-        {'num' : int(num_receive)},
-        {'$set' : {'done' : 1}}
-    )
-    return jsonify({'msg': 'update done!'})
-
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    bucket_list = list(db.bucket.find({},{'_id' : False}))
-    return jsonify({'buckets': bucket_list})
-
-@app.route("/bucket/delete", methods=["POST"])
-def delete():
-    num_delete = request.form['hapus_num']
-    db.bucket.delete_one({'num' : int(num_delete)})
-    return jsonify({'msg': 'delete done!'})
+    db.diary.insert_one(doc)
+    return jsonify({'msg': 'Data berhasil disimpan!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
